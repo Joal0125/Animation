dcfaac3515ccc16414366bb6d2b1f3719b771db8
Joal0125 <jorgetapias@iMac-de-Jorge.local>
2022-08-25 09:09:25 -0500

    agregué variable booleans LEFT, UP, RIGHT y DOWN, y modifiqué el código para poder usarlas

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let x = 100;
 let y = 100;
+let LEFT, UP, RIGHT, DOWN;
 
 function drawBall(x, y, r) {
 	ctx.beginPath();
@@ -11,22 +12,65 @@ function drawBall(x, y, r) {
 	ctx.fillStyle = "red";
 	ctx.fill();
 }
+// use keydown for start de move with the keyboard
 canvas.addEventListener("keydown", function (e) {
 	if (e.keyCode === 37) {
-		x--;
+		LEFT = true;
+		//x--;
 	}
 	if (e.keyCode === 38) {
-		y--;
+		UP = true;
+		//y--;
 	}
 	if (e.keyCode === 39) {
-		x++;
+		RIGHT = true;
+		//x++;
 	}
 	if (e.keyCode === 40) {
-		y++;
+		DOWN = true;
+		//y++;
 	}
 	//console.log(e);
 });
 
+canvas.addEventListener("keyup", function (e) {
+	if (e.keyCode === 37) {
+		LEFT = false;
+		//x--;
+	}
+	if (e.keyCode === 38) {
+		UP = false;
+		//y--;
+	}
+	if (e.keyCode === 39) {
+		RIGHT = false;
+		//x++;
+	}
+	if (e.keyCode === 40) {
+		DOWN = false;
+		//y++;
+	}
+	//console.log(e);
+});
+
+function move() {
+	if (LEFT) {
+		x--;
+	}
+	if (UP) {
+		y--;
+	}
+	if (RIGHT) {
+		x++;
+	}
+	if (DOWN) {
+		y++;
+	}
+}
+
+//to move the ball
 setInterval(function () {
+	ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight); //to erase the ball after move
+	move();
 	drawBall(x, y, 20);
 }, 1000 / 60);
