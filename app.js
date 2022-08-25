2c580ef720d0c917b426231399536d79f5dc109c
Joal0125 <jorgetapias@iMac-de-Jorge.local>
2022-08-25 08:41:19 -0500

    en esta parte agregué código para crear animaciones y poder probar el uso del teclado

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log("Hello word");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let x = 100;
+let y = 100;
+
+function drawBall(x, y, r) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI);
+	ctx.strokeStyle = "black";
+	ctx.stroke();
+	ctx.fillStyle = "red";
+	ctx.fill();
+}
+canvas.addEventListener("keydown", function (e) {
+	if (e.keyCode === 37) {
+		console.log("LEFT");
+	}
+	if (e.keyCode === 38) {
+		console.log("UP");
+	}
+	if (e.keyCode === 39) {
+		console.log("RIGHT");
+	}
+	if (e.keyCode === 40) {
+		console.log("DOWN");
+	}
+	//console.log(e);
+});
+drawBall(x, y, 20);
